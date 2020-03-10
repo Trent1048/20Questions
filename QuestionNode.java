99c3f81cf28e31d120c8e56b392f8b5e61b78e37
Trent1048 <trentbultsma@gmail.com>
2020-03-10 11:19:52 -0700

    Add everything to QuestionNode

--- QuestionNode.java
+++ QuestionNode.java
@@ -1,3 +1,39 @@
 public class QuestionNode {
-	
+
+	private String text;
+	private boolean isQuestion;
+	private QuestionNode yesNode;
+	private QuestionNode noNode;
+
+	// creates a node for a question that points at
+	// 2 other nodes (1 for yes, 1 for no)
+	public QuestionNode(String question, QuestionNode yesNode,
+		QuestionNode noNode) {
+
+		text = question;
+		this.yesNode = yesNode;
+		this.noNode = noNode;
+		isQuestion = true;
+	}
+
+	// creates a node that stores an answer
+	// and points at no other nodes
+	public QuestionNode(String answer) {
+		text = answer;
+		yesNode = null;
+		noNode = null;
+		isQuestion = false;
+	}
+
+	public QuestionNode getYesNode() {
+		return yesNode;
+	}
+
+	public QuestionNode getNoNode() {
+		return noNode;
+	}
+
+	public String getText() {
+		return text;
+	}
 }
