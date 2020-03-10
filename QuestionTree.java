a64fdce0a8138678a47564bcdf282644a336f1c2
Trent1048 <trentbultsma@gmail.com>
2020-03-10 10:45:03 -0700

    Set up all required functions

---
+++ QuestionTree.java
@@ -0,0 +1,26 @@
+public class QuestionTree {
+
+	public QuestionTree(UserInterface ui) {
+
+	}
+
+	public void play() {
+
+	}
+
+	public void save(PrintStream output) {
+
+	}
+
+	public void load(Scanner input) {
+
+	}
+
+	public int totalGames() {
+
+	}
+
+	public int gamesWon() {
+
+	}
+}
